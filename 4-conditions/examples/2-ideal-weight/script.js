8a4339c11f583d0420aa241c1b6c66fb116e378b
Emerson Barreto <emersoncodes@gmail.com>
2024-02-03 19:54:19 -0300

    optimizing if/else condition for ternary operator

--- 4-conditions/examples/2-ideal-weight/script.js
+++ 4-conditions/examples/2-ideal-weight/script.js
@@ -8,12 +8,7 @@ frm.addEventListener("submit", (e)=>{
   const male = frm.inMale.checked
   const height = Number(frm.inHeight.value)
 
-  let peso
-  if(male) {
-    peso = 22 * Math.pow(height, 2)
-  } else {
-    peso = 21 * Math.pow(height, 2)
-  }
+  const peso = male ? 22 * Math.pow(height, 2) : 21 * Math.pow(height, 2);
 
   result.innerText = `${name}, seu peso ideal é: ${peso.toFixed(2)} kg.`
 })
